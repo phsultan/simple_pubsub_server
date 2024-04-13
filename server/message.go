3d1d1b159b6ee3f5c88dfee99ef74286b4309eb5
Philippe Sultan <philippe.sultan@gmail.com>
2024-04-13 17:18:43 +0200

    Add broker

--- server/message.go
+++ server/message.go
@@ -1,6 +1,24 @@
 package simple_pubsub_server
 
 type Message struct {
-	Topic string `json:"topic"`
-	Body  string `json:"body"`
+	topic string
+	body  string
+}
+
+func NewMessage(msg string, topic string) *Message {
+	// Returns the message object
+	return &Message{
+		topic: topic,
+		body:  msg,
+	}
+}
+
+func (m *Message) GetTopic() string {
+	// returns the topic of the message
+	return m.topic
+}
+
+func (m *Message) GetMessageBody() string {
+	// returns the message body.
+	return m.body
 }
